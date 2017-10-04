08c6f4fee4df864f9a275273c4ace3be7098a605
kamcarvalho <carvalhokaam@gmail.com>
2017-10-04 17:48:33 -0300

    Add integration test to remove a tak

--- test/todoList.integration.test.js
+++ test/todoList.integration.test.js
@@ -58,6 +58,23 @@ describe('Integration tests for todoList API', function(done) {
     return axios.get(`/task/${task.id}`)
         .then((response) => {
           expect(response.status).to.equal(200);
+      });
+  });
+
+  it.only('Remove a task by id should return 200',  async function() {
+    let task = new Task(
+      {
+        title: "Buy fruits",
+        create_date: moment(),
+        status: "ongoing",
+        description: "I have to go at"
+      });
+
+    task = await task.save();
+
+    return axios.delete(`/task/${task.id}`)
+        .then((response) => {
+          expect(response.status).to.equal(200);
         });
   });
 });
