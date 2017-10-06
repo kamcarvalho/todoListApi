2c8bae9bf6718707691468a16bce211ad8d0ba11
kamcarvalho <carvalhokaam@gmail.com>
2017-10-05 22:06:36 -0300

    Improving update task integration test

--- test/todoList.integration.test.js
+++ test/todoList.integration.test.js
@@ -61,7 +61,7 @@ describe('Integration tests for todoList API', function(done) {
       });
   });
 
-  it.only('Remove a task by id should return 200',  async function() {
+  it('Remove a task by id should return 200',  async function() {
     let task = new Task(
       {
         title: "Buy fruits",
@@ -75,6 +75,31 @@ describe('Integration tests for todoList API', function(done) {
     return axios.delete(`/task/${task.id}`)
         .then((response) => {
           expect(response.status).to.equal(200);
+    });
+  });
+
+  it('Update a task by id should return 200',  async function() {
+    let task = new Task(
+      {
+        title: "Buy apple",
+        create_date: moment(),
+        status: "pending",
+        description: "I have to go at"
+      });
+
+    task = await task.save();
+
+    return axios.put(`/task/${task.id}`, {
+      title: "Buy juice",
+      create_date: moment(),
+      status: "ongoing",
+      description: "Book an appointment"
+    })
+        .then((response) => {
+          expect(response.status).to.equal(200);
+          expect(response.data).to.have.property('title', 'Buy juice');
+          expect(response.data).to.have.property('status', 'ongoing');
+          expect(response.data).to.have.property('description', 'Book an appointment');
         });
   });
 });
